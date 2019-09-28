d4ed849390332412cc21565affcb535ab0100dda
Zaiyr <zaiyr.00@gmail.com>
2019-09-28 08:54:22 +0600

    tip calculator

---
+++ TipCalculatorController.java
@@ -0,0 +1,51 @@
+import java.math.BigDecimal;
+import java.math.RoundingMode;
+import java.text.NumberFormat;
+import javafx.beans.value.ChangeListener;
+import javafx.beans.value.ObservableValue;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Label;
+import javafx.scene.control.Slider;
+import javafx.scene.control.TextField;
+
+public class TipCalculatorController {
+   private static final NumberFormat currency = 
+      NumberFormat.getCurrencyInstance();
+   private static final NumberFormat percent = 
+      NumberFormat.getPercentInstance();
+   
+   private BigDecimal tipPercentage = new BigDecimal(0.15);
+
+   @FXML 
+   private TextField amountTextField; 
+
+   @FXML
+   private Label tipPercentageLabel; 
+
+   @FXML
+   private Slider tipPercentageSlider;
+
+   @FXML
+   private TextField tipTextField;
+
+   @FXML
+   private TextField totalTextField;
+
+   @FXML
+   private void calculateButtonPressed(ActionEvent e) {
+      try {
+         BigDecimal amount = new BigDecimal(amountTextField.getText());
+         BigDecimal tip = amount.multiply(tipPercentage);
+         BigDecimal total = amount.add(tip);
+
+         tipTextField.setText(currency.format(tip));
+         totalTextField.setText(currency.format(total));
+      }
+      catch (NumberFormatException ex) {
+         amountTextField.setText("Enter amount");
+         amountTextField.selectAll();
+         amountTextField.requestFocus();
+      }
+   }
+}
